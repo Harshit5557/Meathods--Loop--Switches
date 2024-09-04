c271c6f49892760ad80bdc13022e7cb19bcafe2d
Harshit5557 <akhilendra054@gmail.com>
2024-09-04 18:01:36 +0530

    Initial Commit

---
+++ Loops.js
@@ -0,0 +1,5 @@
+var names=["Huracan EVO","Ferrari 812","Porsche 911","Bently Contenental GT","Ferrari SF90 Stradale"];
+for (i = 0;i < names.length; i++) {
+    var text = names[i] + "<br>";
+    console.log(text);
+}
---
+++ Meathods.js
@@ -0,0 +1,8 @@
+var str="hElLo";
+console.log(str.toUpperCase());
+console.log("<br>");
+console.log(str.toLowerCase());
+console.log("<br>");
+console.log(str.startsWith('A'));
+console.log("<br>");
+console.log(str.startsWith('h'));
